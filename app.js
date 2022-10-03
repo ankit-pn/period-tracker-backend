7913f06b841b3af15527b9cfa6e96fc4bebe9c19
ankit-pn <miankitsingh@gmail.com>
2022-10-03 19:12:08 +0530

    created whole backend.

---
+++ app.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const app = express();
+const bodyParser = require('body-parser');
+const dbConnect = require("./dbConnect");
+const Date = require("./periodModel");
+
+dbConnect();
+
+// Curb Cores Error by adding a header here
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+    );
+    next();
+});
+
+
+
+// body parser configuration
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (request, response, next) => {
+    response.json({ message: "Hey! This is your server response!" });
+    next();
+});
+
+
+// register endpoint
+app.post("/postDates", (request, response) => {
+    const date = new Date({
+        userId: request.body.userId,
+        date: request.body.date,
+        note: request.body.note,
+    });
+
+    // save the new user
+    date.save()
+        // return success if the new user is added to the database successfully
+        .then((result) => {
+            response.status(201).send({
+                message: "Date Saved Suceessfully",
+                result,
+            });
+        })
+        // catch error if the new user wasn't added successfully to the database
+        .catch((error) => {
+            response.status(500).send({
+                message: "Error Saving Date",
+                error,
+            });
+        });
+});
+
+// register endpoint
+app.get('/getDates',async (request,response)=>{
+    const userId = request.body.userId;
+    const date = await Date.find({userId:userId});
+    response.json({"dates": date});
+})
+
+
+
+
+
+module.exports = app;
---
+++ periodModel.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const periodSchema = new mongoose.Schema({
+    userId:{
+        type:String,
+        required: [true,'Please provide userId']
+    },
+    date:{
+        type:String ,
+        required: [true,"Please provide Data"]
+    },
+    note:{
+        type:String,
+        required: [true,"Please provide Note"]
+    }
+})
+
+module.exports = mongoose.model.periodList || mongoose.model('periodList',periodSchema);
